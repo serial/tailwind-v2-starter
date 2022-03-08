da0bcc60995861434de749ad8db4f7cce85e17ce
Micha <micha@hoese.biz>
2022-03-08 11:19:08 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      "./*.{php,html,js,less}",
+      "./**/*.{php,html,js,less}"
+    ]
+  },
+  theme: {
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
